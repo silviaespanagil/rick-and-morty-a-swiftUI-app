e6b26f69a641d624b189384c1894dc4929bc8477
Silvia España <silviaespanagil@gmail.com>
2021-11-04 16:11:12 +0100

    Create APIManager File Define a Constants Struct with API Base URL

---
+++ RickAndMorty/RickMorty/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  RickMorty
+//
+//  Created by Silvia España on 4/11/21.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let baseURL = "https://rickandmortyapi.com/api/"
+}
