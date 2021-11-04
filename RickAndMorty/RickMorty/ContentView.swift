be580894f96eff3f50a20fdc3db2372db7ee89c4
Silvia España Gil <81619759+silviaespanagil@users.noreply.github.com>
2021-11-04 13:42:41 +0100

    Create Project

---
+++ RickAndMorty/RickMorty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RickMorty
+//
+//  Created by Silvia España on 4/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RickAndMorty/RickMorty/RickMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyApp.swift
+//  RickMorty
+//
+//  Created by Silvia España on 4/11/21.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
