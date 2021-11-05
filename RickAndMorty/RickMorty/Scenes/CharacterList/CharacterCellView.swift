d71cd6fa7893b639c08ac0af6dc734d156f85012
Silvia España <silviaespanagil@gmail.com>
2021-11-05 12:52:09 +0100

    Create folders and default files to main scenes

---
+++ RickAndMorty/RickMorty/Scenes/CharacterList/CharacterCellView.swift
@@ -0,0 +1,20 @@
+//
+//  CharacterCellView.swift
+//  RickMorty
+//
+//  Created by Silvia España on 5/11/21.
+//
+
+import SwiftUI
+
+struct CharacterCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CharacterCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        CharacterCellView()
+    }
+}
---
+++ RickAndMorty/RickMorty/Scenes/Location/LocationCellView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationCellView.swift
+//  RickMorty
+//
+//  Created by Silvia España on 5/11/21.
+//
+
+import SwiftUI
+
+struct LocationCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationCellView()
+    }
+}
