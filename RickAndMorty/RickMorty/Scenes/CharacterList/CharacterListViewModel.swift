7d3f562addf0a6073d4e9fd887178dbae94db655
Silvia España <silviaespanagil@gmail.com>
2021-11-08 11:07:32 +0100

    Create CharacterListViewModel with the getAllCharacters function

---
+++ RickAndMorty/RickMorty/Scenes/CharacterList/CharacterListViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  CharacterListViewModel.swift
+//  RickMorty
+//
+//  Created by Silvia España on 5/11/21.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CharacterListViewModel: ObservableObject {
+    
+ 
+    @Published public private(set) var characters: [Character] = []
+    
+    @Published public private(set) var showProgressView = false
+    
+    private var cancellable: AnyCancellable?
+    
+    init() {
+        getAllCharacters()
+    }
+    
+    func getAllCharacters() {
+       
+        showProgressView = true
+        
+        cancellable = GetAllCharactesUseCase().execute()
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+                
+                self.showProgressView = false
+                
+                switch completion {
+                case .finished:
+                    break
+                case .failure:
+                    break
+                }
+                
+            }, receiveValue: {(characters: [Character]) in
+                
+                self.characters = characters
+            })
+        
+    }
+    
+}
