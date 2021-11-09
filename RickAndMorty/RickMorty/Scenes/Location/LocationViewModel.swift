8626944b4f1b82cbd5a07fbb2c41aa486f21bbb3
Silvia España <silviaespanagil@gmail.com>
2021-11-09 15:26:02 +0100

    Create ViewModel for the Location View in order to manage data and API Call

---
+++ RickAndMorty/RickMorty/Scenes/Location/LocationViewModel.swift
@@ -0,0 +1,51 @@
+//
+//  LocationViewModel.swift
+//  RickMorty
+//
+//  Created by Silvia España on 9/11/21.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class LocationViewModel: ObservableObject {
+    
+ 
+    @Published public private(set) var locations: [Location] = []
+    
+    @Published public private(set) var showProgressView = false
+    
+    let dimension = "Dimension: "
+    
+    private var cancellable: AnyCancellable?
+    
+    init() {
+        getLocation()
+    }
+    
+    func getLocation() {
+       
+        showProgressView = true
+        
+        cancellable = GetLocationUseCase().execute()
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+                
+                self.showProgressView = false
+                
+                switch completion {
+                case .finished:
+                    break
+                case .failure:
+                    break
+                }
+                
+            }, receiveValue: {(locations: [Location]) in
+                
+                self.locations = locations
+            })
+        
+    }
+    
+}
