9931d48daff29a5ec06244f4500fda6c5fa1a527
Silvia España <silviaespanagil@gmail.com>
2021-11-09 17:04:00 +0100

    Test Character Detail Unit Test -- passed

---
+++ RickAndMorty/RickMortyTests/UseCases/CharacterDetailUseCaseUnitTest.swift
@@ -0,0 +1,41 @@
+//
+//  CharacterDetailUseCaseUnitTest.swift
+//  RickMortyTests
+//
+//  Created by Silvia España on 9/11/21.
+//
+
+import XCTest
+@testable import RickMorty
+
+class CharacterDetailUseCaseUnitTest: XCTestCase {
+
+    var sut: GetCharacterDetailUseCase?
+    
+    override func setUpWithError() throws {
+        
+        try super.setUpWithError()
+    }
+    
+    override func tearDownWithError() throws {
+        sut = nil
+        try super.tearDownWithError()
+    }
+    
+    func testGetCharacterDetailIsCalled() throws {
+        
+        // Given
+        
+        let repository = MockCharacterRepository()
+        sut = GetCharacterDetailUseCase(repository: repository)
+        
+        // When
+        
+        _ = sut!.execute(id: 1)
+        
+        // Then
+        
+        XCTAssertTrue(repository.isGetCharacterDetailCalled)
+        
+    }
+}
