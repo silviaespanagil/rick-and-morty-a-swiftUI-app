9454ff905e756acf8d69aa3ae99f4a70cc358175
Silvia España <silviaespanagil@gmail.com>
2021-11-09 15:26:54 +0100

    Add list of Locations in LocationView

--- RickAndMorty/RickMorty/Scenes/Location/LocationView.swift
+++ RickAndMorty/RickMorty/Scenes/Location/LocationView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct LocationCellView: View {
+    
+    @StateObject var viewModel: LocationViewModel
+    
     var body: some View {
-    Text("Locations")
         
+        NavigationView{
+            
+            List {
+                
+                ForEach(viewModel.locations) { location in
+                    
+                    VStack(alignment: .leading, spacing:10) {
+                        
+                        Spacer()
+                        Text(location.name)
+                        
+                        VStack(alignment: .leading) {
+                            
+                            Text(location.type)
+                            Text("\(viewModel.dimension)\(location.dimension)")
+                            
+                        }
+                        .font(.footnote)
+                        Spacer()
+                    }
+                }
+            }
+        }
     }
 }
 
 struct LocationCellView_Previews: PreviewProvider {
     static var previews: some View {
-        LocationCellView()
+        LocationCellView(viewModel: LocationViewModel())
     }
 }
