6e11081861f0560368182217811af8d582c11d5a
Silvia España <silviaespanagil@gmail.com>
2021-11-09 16:40:19 +0100

    Added EpisodeViewModel

---
+++ RickAndMorty/RickMorty/Scenes/Episodes/EpisodeViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  EpisodeViewModel.swift
+//  RickMorty
+//
+//  Created by Silvia España on 9/11/21.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class EpisodeViewModel: ObservableObject {
+    
+ 
+    @Published public private(set) var episodes: [Episode] = []
+    
+    @Published public private(set) var showProgressView = false
+    
+    private var cancellable: AnyCancellable?
+    
+    init() {
+        getEpisode()
+    }
+    
+    func getEpisode() {
+       
+        showProgressView = true
+        
+        cancellable = GetEpisodeUseCase().execute()
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+                
+                self.showProgressView = false
+                
+                switch completion {
+                case .finished:
+                    break
+                case .failure:
+                    break
+                }
+                
+            }, receiveValue: {(episodes: [Episode]) in
+                
+                self.episodes = episodes
+            })
+        
+    }
+    
+}
