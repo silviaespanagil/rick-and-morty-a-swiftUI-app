84a9536f316cf149c98309e069efad203e7a4bfa
Silvia España <silviaespanagil@gmail.com>
2021-11-09 10:49:18 +0100

    Created the Unit Test for GetAllCharactersUse Case

---
+++ RickAndMorty/RickMortyTests/UseCases/GetAllCharactersUseCaseUnitTest.swift
@@ -0,0 +1,32 @@
+//
+//  GetAllCharactersUseCaseUnitTest.swift
+//  RickMortyTests
+//
+//  Created by Silvia España on 9/11/21.
+//
+
+import XCTest
+@testable import RickMorty
+
+class GetAllCharactersUseCaseUnitTest: XCTestCase {
+    
+  //  var sut = GetAllCharactersUseCase?
+    
+    override func setUpWithError() throws {
+        
+        try super.setUpWithError()
+    }
+    
+    override func tearDownWithError() throws {
+       // sut = nil
+        try super.tearDownWithError()
+    }
+    
+    func testGetAllCharactersIsCalled() throws {
+        
+        
+    }
+    
+}
+
+
