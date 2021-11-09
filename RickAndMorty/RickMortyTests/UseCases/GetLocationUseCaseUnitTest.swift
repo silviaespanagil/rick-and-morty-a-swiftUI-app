3e68f65cf168b6ecb001656c89acb565384f9359
Silvia España <silviaespanagil@gmail.com>
2021-11-09 15:50:02 +0100

    Add test for the Location Use Case

---
+++ RickAndMorty/RickMortyTests/UseCases/GetLocationUseCaseUnitTest.swift
@@ -0,0 +1,38 @@
+//
+//  GetLocationUseCaseUnitTest.swift
+//  RickMortyTests
+//
+//  Created by Silvia España on 9/11/21.
+//
+
+import XCTest
+@testable import RickMorty
+
+class GetLocationUseCaseUnitTest: XCTestCase {
+    
+    var sut: GetLocationUseCase?
+
+    override func setUpWithError() throws {
+        
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        
+        sut = nil
+        try super.tearDownWithError()
+    }
+
+    func testGetLocationsIsCalled() throws {
+        
+        // Given
+        let repository = MockLocationRepository()
+        sut = GetLocationUseCase(repository: repository)
+        
+        // When
+        _ = sut!.execute()
+        
+        // Then
+        XCTAssertTrue(repository.isGetLocationCalled)
+    }
+}
