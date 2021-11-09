d88912be7c1dfb14304f348e64a89638d51672a4
Silvia España <silviaespanagil@gmail.com>
2021-11-09 13:51:00 +0100

    Create location UseCase

---
+++ RickAndMorty/RickMorty/UseCases/GetLocationUseCase.swift
@@ -0,0 +1,24 @@
+//
+//  GetLocationUseCase.swift
+//  RickMorty
+//
+//  Created by Silvia España on 9/11/21.
+//
+
+import Foundation
+import Combine
+
+class GetLocationUseCase {
+    
+    private let repository: LocationRepository
+    
+    init(repository: LocationRepository = LocationRepositoryImplementation()) {
+        
+        self.repository = repository
+    }
+    
+    func execute() -> AnyPublisher<[Location], Error> {
+        
+        repository.getLocation()
+    }
+}
