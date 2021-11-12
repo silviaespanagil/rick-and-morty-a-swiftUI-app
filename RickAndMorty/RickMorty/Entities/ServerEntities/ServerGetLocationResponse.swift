88011ed71e893e9add87307a0d23936635c3daa1
Silvia España <silviaespanagil@gmail.com>
2021-11-12 12:24:49 +0100

    Updated Server Location Response to add new entity: Residents

--- RickAndMorty/RickMorty/Entities/ServerEntities/ServerGetLocationResponse.swift
+++ RickAndMorty/RickMorty/Entities/ServerEntities/ServerGetLocationResponse.swift
@@ -12,13 +12,20 @@ struct ServerGetLocationResponse: Codable {
     let id: Int
     let name: String
     let type: String
+    let residents: [ServerResidentResponse]?
     let dimension: String
     
     func converToEntity() -> Location {
         
+        var res: [Resident] = []
+        if let serverResidents = residents {
+            res = serverResidents.map({ $0.convertToEntity()})
+        }
+        
         return Location(id: id,
                          name: name,
                          type: type,
+                        residents: res,
                          dimension: dimension)
     }
 }
