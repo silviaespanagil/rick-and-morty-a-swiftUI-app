5abb409480a4c1f2756b5bd10d5f5d7131ebb120
Silvia España <silviaespanagil@gmail.com>
2021-11-16 13:04:52 +0100

    Add the getEpisodeDetail function to the viewModel

--- RickAndMorty/RickMorty/Scenes/EpisodeDetail/EpisodeDetailViewModel.swift
+++ RickAndMorty/RickMorty/Scenes/EpisodeDetail/EpisodeDetailViewModel.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import Combine
+import SwiftUI
+
+class EpisodeDetailViewModel: ObservableObject {
+    
+    @Published public private(set) var episode: Episode
+    @Published public private(set) var showProgressView = false
+    
+    private var cancellable: AnyCancellable?
+    
+    init(episode: Episode){
+        
+        self.episode = episode
+        
+        self.getEpisodeDetail()
+    }
+    
+    func getEpisodeDetail() {
+        
+        showProgressView = true
+        cancellable = GetEpisodeDetailUseCase().execute(id: episode.id)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+                
+                self.showProgressView = false
+                
+                switch completion {
+                case .finished:
+                    break
+                case .failure:
+                    break
+                }
+                
+            }, receiveValue: { (episode: Episode) in
+                
+                self.episode = episode
+            })
+    }
+}
