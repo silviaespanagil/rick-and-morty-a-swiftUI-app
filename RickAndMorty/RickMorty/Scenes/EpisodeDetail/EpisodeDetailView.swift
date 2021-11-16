d37fde506a0f0b4f4738ddd49038938a8c1efb30
Silvia España <silviaespanagil@gmail.com>
2021-11-16 12:43:40 +0100

    Create new files for episodeDetail View and ViewModel

---
+++ RickAndMorty/RickMorty/Scenes/EpisodeDetail/EpisodeDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  EpisodeDetailView.swift
+//  RickMorty
+//
+//  Created by Silvia España on 16/11/21.
+//
+
+import SwiftUI
+
+struct EpisodeDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EpisodeDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        EpisodeDetailView()
+    }
+}
---
+++ RickAndMorty/RickMorty/Scenes/EpisodeDetail/EpisodeDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  EpisodeDetailViewModel.swift
+//  RickMorty
+//
+//  Created by Silvia España on 16/11/21.
+//
+
+import Foundation
