e43edd9b999779127e02a6e924e7ada1ac37b909
Silvia España <silviaespanagil@gmail.com>
2021-11-16 17:45:46 +0100

    Test getAllEpisodesError and EpisodesDetail in repository implementation

--- RickAndMorty/RickMortyTests/Repositories/EpisodeTest/EpisodesRepositoryImplementationUnitTest.swift
+++ RickAndMorty/RickMortyTests/Repositories/EpisodeTest/EpisodesRepositoryImplementationUnitTest.swift
@@ -25,13 +25,13 @@ class EpisodesRepositoryImplementationUnitTest: XCTestCase {
         
         try super.setUpWithError()
     }
-
+    
     override func tearDownWithError() throws {
         
         sut = nil
         try super.tearDownWithError()
     }
-
+    
     func testGetAllEpisodesOK() throws {
         
         // Given
@@ -60,8 +60,85 @@ class EpisodesRepositoryImplementationUnitTest: XCTestCase {
                 XCTAssertEqual(episode.count, 1)
                 XCTAssertEqual(episode.first?.id, 1)
                 XCTAssertEqual(episode.first?.name, "Pilot")
-                XCTAssertEqual(episode.first?.airDate, "Episode air date")
-                XCTAssertEqual(episode.first?.episode, "Episode number")
+                XCTAssertEqual(episode.first?.airDate, "December 2, 2013")
+                XCTAssertEqual(episode.first?.episode, "S01E01")
+            })
+        
+        wait(for: [exp], timeout: timeoutTime)
+        
+        // Then
+        XCTAssertNotNil(cancellable)
+    }
+    
+    func testGetAllEpisodesError() throws {
+        
+        // Given
+        let endpoint = "episode"
+        let session = getEpisodeSession(statusCode: failureStatusCode, endpoint: endpoint)
+        
+        let remote = EpisodeRemoteDataSource(baseURL: baseUrlString, session: session)
+        
+        sut = EpisodeRepositoryImplementation(remoteDataSource: remote)
+        
+        let exp = self.expectation(description: "expected values")
+        
+        // When
+        cancellable = sut!.getEpisode()
+            .sink(receiveCompletion: { completion in
+                
+                switch completion {
+                case .finished:
+                    break
+                case .failure:
+                    exp.fulfill()
+                }
+                
+            }, receiveValue: { episode in
+                
+                // nothing
+            })
+        
+        wait(for: [exp], timeout: timeoutTime)
+        
+        // Then
+        XCTAssertNotNil(cancellable)
+    }
+    
+    func testGetEpisodeDetail() throws {
+        
+        // Given
+        let endpoint = "episode"
+        let id = 1
+        let session = getEpisodeDetailSession(statusCode: sucessStatusCode, endpoint: endpoint, id: id)
+        
+        let remote = EpisodeRemoteDataSource(baseURL: baseUrlString, session: session)
+        
+        sut = EpisodeRepositoryImplementation(remoteDataSource: remote)
+        
+        let exp = self.expectation(description: "expected values")
+        
+        // When
+        cancellable = sut!.getEpisodeDetail(id: id)
+            .sink(receiveCompletion: { completion in
+                
+                switch completion {
+                case .finished:
+                    exp.fulfill()
+                case .failure:
+                    break
+                }
+                
+            }, receiveValue: { episode in
+                
+                XCTAssertEqual(episode.id, 1)
+                XCTAssertEqual(episode.name, "Pilot")
+                XCTAssertEqual(episode.airDate, "December 2, 2013")
+                XCTAssertEqual(episode.episode, "S01E01")
+                XCTAssertEqual(episode.characters, [
+                    "https://rickandmortyapi.com/api/character/1",
+                    "https://rickandmortyapi.com/api/character/2",
+                ])
+                
             })
         
         wait(for: [exp], timeout: timeoutTime)
@@ -98,6 +175,31 @@ extension EpisodesRepositoryImplementationUnitTest {
         return session
     }
     
+    func getEpisodeDetailSession(statusCode: Int, endpoint: String, id: Int) -> URLSession {
+        
+        // URL we expect to call
+        let url = URL(string: "http://jsonplaceholder.typicode.com/\(endpoint)/\(id)")
+        
+        // data we expect to receive
+        let data = getEpisodeDetailData()
+        
+        // attach that to some fixed data in our protocol handler
+        URLProtocolMock.testURLs = [url: data]
+        URLProtocolMock.response = HTTPURLResponse(url: URL(string: "http://jsonplaceholder.typicode.com:8080")!,
+                                                   statusCode: statusCode,
+                                                   httpVersion: nil,
+                                                   headerFields: nil)
+        
+        // now setup a configuration to use our mock
+        let config = URLSessionConfiguration.ephemeral
+        config.protocolClasses = [URLProtocolMock.self]
+        
+        // and ceate the URLSession form that
+        let session = URLSession(configuration: config)
+        
+        return session
+    }
+    
     func getEpisodeData() -> Data {
         
         let dataString = """
@@ -107,28 +209,11 @@ extension EpisodesRepositoryImplementationUnitTest {
                               {
                                  "id":1,
                                  "name":"Pilot",
-                                 "air_date":"Episode air date",
-                                 "episode":"Episode number",
+                                 "air_date":"December 2, 2013",
+                                 "episode":"S01E01",
                                  "characters":[
                                     "https://rickandmortyapi.com/api/character/1",
                                     "https://rickandmortyapi.com/api/character/2",
-                                    "https://rickandmortyapi.com/api/character/35",
-                                    "https://rickandmortyapi.com/api/character/38",
-                                    "https://rickandmortyapi.com/api/character/62",
-                                    "https://rickandmortyapi.com/api/character/92",
-                                    "https://rickandmortyapi.com/api/character/127",
-                                    "https://rickandmortyapi.com/api/character/144",
-                                    "https://rickandmortyapi.com/api/character/158",
-                                    "https://rickandmortyapi.com/api/character/175",
-                                    "https://rickandmortyapi.com/api/character/179",
-                                    "https://rickandmortyapi.com/api/character/181",
-                                    "https://rickandmortyapi.com/api/character/239",
-                                    "https://rickandmortyapi.com/api/character/249",
-                                    "https://rickandmortyapi.com/api/character/271",
-                                    "https://rickandmortyapi.com/api/character/338",
-                                    "https://rickandmortyapi.com/api/character/394",
-                                    "https://rickandmortyapi.com/api/character/395",
-                                    "https://rickandmortyapi.com/api/character/435"
                                  ],
                                  "url":"https://rickandmortyapi.com/api/episode/1",
                                  "created":"2017-11-10T12:56:33.798Z"
@@ -137,7 +222,29 @@ extension EpisodesRepositoryImplementationUnitTest {
                     }
                     
                     """
-
+        
+        return Data(dataString.utf8)
+    }
+    
+    func getEpisodeDetailData() -> Data {
+        
+        let dataString = """
+                     { "results":
+                   {
+                   "id":1,
+                   "name":"Pilot",
+                   "air_date":"December 2, 2013",
+                   "episode":"S01E01",
+                   "characters":[
+                   "https://rickandmortyapi.com/api/character/1",
+                   "https://rickandmortyapi.com/api/character/2",
+                   ],
+                   "url":"https://rickandmortyapi.com/api/episode/1",
+                   "created":"2017-11-10T12:56:33.798Z"
+                   }
+                   }
+                   """
+        
         return Data(dataString.utf8)
     }
 }
