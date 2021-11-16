7f2464b1316770cc7d10b97557815abd0e9a0cf9
Silvia España <silviaespanagil@gmail.com>
2021-11-16 16:09:11 +0100

    Fix test, adding episode to the data

--- RickAndMorty/RickMortyTests/Repositories/CharacterTest/CharacterRepositoryImplementationUnitTest.swift
+++ RickAndMorty/RickMortyTests/Repositories/CharacterTest/CharacterRepositoryImplementationUnitTest.swift
@@ -226,6 +226,10 @@ extension CharacterRepositoryImplementationUnitTest {
                                     "name": "Citadel of Ricks",
                                     "url": "location url"
                                 },
+                                "episode":[
+                                    "https://rickandmortyapi.com/api/episode/1",
+                                    "https://rickandmortyapi.com/api/episode/2",
+                                           ],
                                 "url": "character url"
                             }
                         ]
@@ -256,6 +260,10 @@ extension CharacterRepositoryImplementationUnitTest {
                           "url":"https://rickandmortyapi.com/api/location/3"
                        },
                        "image":"https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+                       "episode":[
+                          "https://rickandmortyapi.com/api/episode/1",
+                          "https://rickandmortyapi.com/api/episode/2",
+                       ],
                        "url":"https://rickandmortyapi.com/api/character/1",
                        "created":"2017-11-04T18:48:46.250Z"
                     }
